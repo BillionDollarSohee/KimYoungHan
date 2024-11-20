7f3802a80d31be50bb728942c26446356d9550a8
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2024-11-20 17:03:04 +0900

    feature/순수자바memberCode

---
+++ src/main/java/youngHan/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package youngHan.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); //동시성이슈로 컨커런트 해쉬맵을 사용해야함
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+
+}
