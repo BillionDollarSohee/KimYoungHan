cf95a7850ef1067f715cfccf43305206560af615
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2024-11-21 15:04:32 +0900

    test/mainMethod OrderTest

--- src/main/java/youngHan/core/OrderApp.java
+++ src/main/java/youngHan/core/OrderApp.java
@@ -1,4 +1,27 @@
 package youngHan.core;
 
+import youngHan.core.member.Grade;
+import youngHan.core.member.Member;
+import youngHan.core.member.MemberService;
+import youngHan.core.member.MemberServiceImpl;
+import youngHan.core.order.Order;
+import youngHan.core.order.OrderService;
+import youngHan.core.order.OrderServiceImpl;
+
 public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order = " + order.calculatePrice());
+
+    }
 }
