0d3014a1989d8b80c0c958af0afef24195c8f990
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2025-01-18 02:29:12 +0900

    refactor/@MainDiscountPolicy를 생성하여 텍스트를 직접 조회하지 않고 애너테이션으로 조회하게끔 변경

--- src/main/java/youngHan/core/discount/RateDiscountPolicy.java
+++ src/main/java/youngHan/core/discount/RateDiscountPolicy.java
@@ -1,13 +1,13 @@
 package youngHan.core.discount;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import youngHan.core.annotation.MainDiscountPolicy;
 import youngHan.core.member.Grade;
 import youngHan.core.member.Member;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+@MainDiscountPolicy
 //@Primary 를 사용하면 의존 주입시 우선순위를 가지게 되지만 Qualifier 보다는 우선순위가 낮다.
 public class RateDiscountPolicy implements DiscountPolicy {
 
