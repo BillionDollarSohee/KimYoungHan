32990f8b26d06a6ca46850f1884a9c171fca9eb2
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2025-01-18 02:19:24 +0900

    test/configuration테스트를 위한 테스트 삭제, refactor/2개이상의 빈이 조회될 때를 대비

--- src/main/java/youngHan/core/discount/RateDiscountPolicy.java
+++ src/main/java/youngHan/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,14 @@
 package youngHan.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import youngHan.core.member.Grade;
 import youngHan.core.member.Member;
 
 @Component
+@Qualifier("mainDiscountPolicy")
+//@Primary 를 사용하면 의존 주입시 우선순위를 가지게 되지만 Qualifier 보다는 우선순위가 낮다.
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
